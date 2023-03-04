176a57f222ccbfb9cb40cf53f78de9b1d9dd400c
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-04 13:35:36 +0100

    Added polymorphism to WebDriverTest

--- scr/drivers/WebDriverTest.java
+++ scr/drivers/WebDriverTest.java
@@ -2,12 +2,33 @@ package drivers;
 
 public class WebDriverTest {
     public static void main(String[] args) {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get();
-        chromeDriver.findElementBy();
+        WebDriver driver = getDriver("chrome");
+        driver.get();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
 
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
+
+      /*  FirefoxDriver firefoxDriver = new FirefoxDriver();
         firefoxDriver.get();
         firefoxDriver.findElementBy();
+        firefoxDriver.findElementBy();
+        firefoxDriver.findElementBy();
+        firefoxDriver.findElementBy();
+        firefoxDriver.findElementBy(); */
+
+    }
+
+    private static WebDriver getDriver(String name) {
+        if (name.equals("chrome")) {
+            return new ChromeDriver();
+        } else if (name.equals("firefox")) {
+            return new FirefoxDriver();
+
+        }
+        return null;
     }
 }
